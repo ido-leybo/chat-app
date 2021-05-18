8b1f9d3fc35ab4c689a22be2fc125840cc984ba8
Ido Leybovitch <idoleybo01@gmail.com>
2021-05-18 10:26:23 +0300

    initial commit with connection to firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import SignIn from "./components/SignIn";
+import HomePage from "./components/HomePage";
+import firebase from "firebase";
+import "firebase/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC13dhProRqafi7YzoAPE3E9Cwa695Yx6Q",
+  authDomain: "chat-app-e554f.firebaseapp.com",
+  projectId: "chat-app-e554f",
+  storageBucket: "chat-app-e554f.appspot.com",
+  messagingSenderId: "1079674620892",
+  appId: "1:1079674620892:web:e2d7c2307046753e7cdd19",
+  measurementId: "G-9QLKEY2C2K",
+});
+
+const auth = firebase.auth();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>{user ? <HomePage user={user} /> : <SignIn />}</section>
     </div>
   );
 }
